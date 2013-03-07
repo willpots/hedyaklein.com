3735d8900b9a47e65e79c9b7173ea51569223f14
Will Potter <will@wpotter.me>
2013-03-07 16:45:20 -0500

    added version 1.0

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$("#animation-b").click(function(e) {
+		$('.article').addClass('hidden');
+		$('#animation').removeClass('hidden');
+	});
+	$("#collaboration-b").click(function(e) {
+		$('.article').addClass('hidden');
+		$('#collaboration').removeClass('hidden');
+	});
+	$("#installation-b").click(function(e) {
+		$('.article').addClass('hidden');
+		$('#installation').removeClass('hidden');
+	});
+	$("#public-work-b").click(function(e) {
+		$('.article').addClass('hidden');
+		$('#public-work').removeClass('hidden');
+	});
+	$("#paper-b").click(function(e) {
+		$('.article').addClass('hidden');
+		$('#paper').removeClass('hidden');
+	});
+
+
+});
